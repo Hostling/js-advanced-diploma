08b3a2aa724cb45b40c7b91e0aa0e559bcc7c705
Maksim Bondarev <grannas@xaker.ru>
2020-01-13 11:57:09 +0400

    Отрисовка команд

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -5,11 +5,14 @@ import Undead from './Undead';
 import Vampire from './Vampire';
 import Daemon from './Daemon';
 import { generateTeam } from './generators'
+import PositionedCharacter from './PositionedCharacter';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.charactersCount = 3;
+    this.maxLevel = 4;
     this.userTeam = [];
     this.enemyTeam = [];
   }
@@ -18,8 +21,46 @@ export default class GameController {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
     this.gamePlay.drawUi('prairie');
-    this.userTeam = generateTeam([Bowman, Swordsman, Magician], 4, 3);
-    this.enemyTeam = generateTeam([Undead, Vampire, Daemon], 4, 3);
+
+    const userPositions = Array.from(this.getStartPositions(this.gamePlay.boardSize, 'user'));
+    const enemyPositions = Array.from(this.getStartPositions(this.gamePlay.boardSize, 'enemy'));
+    this.userTeam = generateTeam([Bowman, Swordsman, Magician], this.maxLevel, this.charactersCount);
+    this.enemyTeam = generateTeam([Undead, Vampire, Daemon], this.maxLevel, this.charactersCount);
+    this.userTeam = this.userTeam.map((elem) => {
+      return new PositionedCharacter(elem.value, userPositions.shift())
+    });
+    this.enemyTeam = this.enemyTeam.map((elem) => {
+      return new PositionedCharacter(elem.value, enemyPositions.shift())
+    });
+    this.gamePlay.redrawPositions(this.userTeam.concat(this.enemyTeam));
+  }
+
+  getStartPositions(size, side) {
+    const positions = [];
+    const positionsSet = new Set();
+    if(side === 'user') {
+      positions.push(0);
+      positions.push(1);
+      for(let i = 0; i < size**2; i++) {
+        if(i % size === 0) {
+          positions.push(i);
+          positions.push(i+1);
+        }
+      }
+    } else {
+      for(let i = 0; i < size**2; i++) {
+        if((i + 1) % size === 0) {
+          positions.push(i);
+          positions.push(i-1);
+        }
+      }
+    }
+
+    while(positionsSet.size !== this.charactersCount) {
+      positionsSet.add(positions[Math.floor(Math.random() * Math.floor(positions.length))])
+    }
+
+    return positionsSet;
   }
 
   onCellClick(index) {
